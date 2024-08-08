ea556cacc585e07a744aa7da7570917496cb1485
Zohar Singer <injapanthree@gmail.com>
2024-08-08 23:35:06 +0100

    better default theme

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Hero from "./components/Hero"
 import MainContent from "./components/MainContent"
 
 const Portfolio = () => {
 	const [theme, setTheme] = useState("dark" as "dark" | "light")
+	const [isLoaded, setIsLoaded] = useState(false)
+
+	useEffect(() => {
+		const localTheme = localStorage.getItem("theme")
+		if (localTheme === "dark" || localTheme === "light") {
+			setTheme(localTheme as "dark" | "light")
+		} else {
+			// no theme found in local storage, search for system preference
+			const lightModeMediaQuery = window.matchMedia(
+				"(prefers-color-scheme: light)"
+			)
+
+			// default to dark if both false
+			setTheme(lightModeMediaQuery.matches ? "light" : "dark")
+		}
+
+		setIsLoaded(true)
+	}, [])
+
+	useEffect(() => {
+		if (!isLoaded) return
+		localStorage.setItem("theme", theme)
+	}, [theme, isLoaded])
 
 	return (
 		<div
